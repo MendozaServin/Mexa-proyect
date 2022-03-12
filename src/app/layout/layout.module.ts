5663cce5d48747e13d6651f3195d93ca0df7cc4b
noeli <noe.rmorales98@gmail.com>
2022-03-11 22:34:10 -0500

    jsdbasaksn

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,12 +4,14 @@ import { HeaderComponent } from './header/header.component';
 import { LayoutComponent } from './layout.component';
 import {RouterModule} from "@angular/router";
 import {RoutesModule} from "../routes/routes.module";
+import { Headerv2Component } from './headerv2/headerv2.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    Headerv2Component,
   ],
   exports: [
     HeaderComponent
